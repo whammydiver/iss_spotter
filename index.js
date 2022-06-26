9f8b730716dfb0b7108a9016b6a5e436d6e2c266
Paul Taylor <paul@whammydiver.com>
2022-06-26 15:30:48 -0400

    ISS complete

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
-
-// let ipString = '';
-// let geoCoords = {};
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-//   ipString = ip
-// });
-
-// fetchCoordsByIP(ipString, (error, coords) => {
-//   if (error) {
-//     console.log('sad times', error);
-//     return;
-//   }
-//   console.log(coords)
-//   geoCoords = coords;
-// });
-
-// fetchISSFlyOverTimes(geoCoords, (error, times) => {
-//   if (error) {
-//     console.log('more sad times', error);
-//     return;
-//   }
-//   console.log(times);
-// })
-
-// index.js
-
 const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
-  console.log(passTimes);
+  printPassTimes(passTimes);
 });
